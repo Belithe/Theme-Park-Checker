bf6a24ca44c80f4f1d10ceb4de48a3395e1fbb41
Unknown <JunkmailBelith@gmail.com>
2022-01-27 16:32:58 +0100

    Creating access to JS and CSS

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 WebDev1Project/BaseHTML/
+web: vendor/bin/heroku-php-apache2 WebDev1Project/
--- WebDev1Project/index.php
+++ WebDev1Project/index.php
@@ -3,6 +3,6 @@
 
 //this file is a redirect, this should never be seen or called outside of initial load
 
-header("Location: Views/HomeView.php");
+header("Location: BaseHTML/Views/HomeView.php");
 
 //header("Location: DataHandlers/install.php");
