21c0d887bef4f171e628cc9e50592cc527075224
Unknown <JunkmailBelith@gmail.com>
2022-01-30 20:40:31 +0100

    Removal of test msg api tests

--- WebDev1Project/API/parks.php
+++ WebDev1Project/API/parks.php
@@ -1,22 +1,16 @@
 <?php
 
 try {
-    echo "-2-";
+    
     require_once $_SERVER['DOCUMENT_ROOT'] . '/BaseHTML/Models/Park.php';
-    echo "-1-";
     require_once $_SERVER['DOCUMENT_ROOT'] . '/BaseHTML/Controllers/ParkController.php';
-    echo "0";
 
     $controller = new ParkController();
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        echo "1";
         $parks = $controller->GetAllParks();
-        echo "2";
         $json = json_encode($parks);
-        echo "3";
         header('Content-type:application/json; charset=utf-8');
-        echo "4";
         echo $json;
     } else {
         echo "Invalid request type";
